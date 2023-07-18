5bd8498ed00bf54ea4895c887406c0345d73601f
Mina <blobcattv@gmail.com>
2023-07-19 00:25:11 +0200

    add bouncing powerups

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ const config = {
     height: 400,
     backgroundColor: 0x20395,
     pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    },
     zoom: 2,
     scene: [Scene1, Scene2]
 }
--- scene2.js
+++ scene2.js
@@ -4,17 +4,14 @@ class Scene2 extends Phaser.Scene {
     }
   
     create() {
-  
       this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
       this.background.setOrigin(0, 0);
       this.background.setScale(2);
   
-      // 0.1 change from image to sprite
       this.ship1 = this.add.sprite(config.width / 2 - 50, config.height / 2, "ship");
       this.ship2 = this.add.sprite(config.width / 2, config.height / 2, "ship2");
       this.ship3 = this.add.sprite(config.width / 2 + 50, config.height / 2, "ship3");
   
-      // 0.2 create animations
       this.anims.create({
         key: "ship1_anim",
         frames: this.anims.generateFrameNumbers("ship"),
@@ -41,6 +38,27 @@ class Scene2 extends Phaser.Scene {
         repeat: 0,
         hideOnComplete: true
       });
+
+      this.anims.create({
+        key: "red",
+        frames: this.anims.generateFrameNumbers("power-up", {
+            start: 0,
+            end: 1
+        }),
+        frameRate: 20,
+        repeat: -1
+      });
+
+      this.anims.create({
+        key: "gray",
+        frames: this.anims.generateFrameNumbers("power-up", {
+            start: 2,
+            end: 3
+        }),
+        frameRate: 20,
+        repeat: -1
+      });
+
   
       this.ship1.play("ship1_anim");
       this.ship2.play("ship2_anim");
@@ -56,17 +74,32 @@ class Scene2 extends Phaser.Scene {
         font: "25px Arial",
         fill: "yellow"
       });
-  
+
+      this.powerUps = this.physics.add.group();
+      const maxObjects = 4;
+      for (var i = 0; i <= maxObjects; i++) {
+        const powerUp = this.physics.add.sprite(16, 16, "power-up");
+        this.powerUps.add(powerUp);
+        powerUp.setRandomPosition(0, 0, game.config.width, game.config.height)
+
+        if (Math.random() > 0.5) {
+            powerUp.play("red");
+        } else {
+            powerUp.play("gray");
+        }
+
+        powerUp.setVelocity(100, 100)
+        powerUp.setCollideWorldBounds(true);
+        powerUp.setBounce(1);
+      }
     }
   
     update() {
-  
       this.moveShip(this.ship1, 1);
       this.moveShip(this.ship2, 2);
       this.moveShip(this.ship3, 3);
   
-      this.background.tilePositionY -= 0.5;
-  
+      this.background.tilePositionY -= 0.2;
     }
   
     moveShip(ship, speed) {
