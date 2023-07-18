14ace48cb8d9090aa5aa5dd32eee2527faca5893
Mina <blobcattv@gmail.com>
2023-07-18 22:13:29 +0200

    add spritesheets and add destroyable moving ships

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const config = {
-    width: 1000,
-    height: 700,
+    width: 400,
+    height: 400,
     backgroundColor: 0x20395,
+    pixelArt: true,
+    zoom: 2,
     scene: [Scene1, Scene2]
 }
 
